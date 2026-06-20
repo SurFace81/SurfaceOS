b6e4f456122c72145dece5749bb7684eb776b220
SurFace81 <sur.f4ce@yandex.ru>
2026-06-20 18:48:40 +0300

    memory::memcpy fix

--- src/include/mm/memory.h
+++ src/include/mm/memory.h
@@ -9,7 +9,7 @@ namespace memory
     void        init(uint64_t mem_size);
     uint64_t    total(void);
     void        memset(uint8_t* addr, char value, uint64_t size);
-    void        memcpy(uint8_t* src, uint8_t* dst, uint64_t size);
+    void        memcpy(uint8_t* dst, uint8_t* src, uint64_t size);
 } // namespace memory
 
 #endif // MEMORY_H
